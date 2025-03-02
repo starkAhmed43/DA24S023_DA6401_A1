608b3347f95bec95150c911c48dde6de6e468f93
Adhil Ahmed <34025370+starkAhmed43@users.noreply.github.com>
2025-03-02 17:41:34 +0530

    Overhaul backprop code and add weight update

--- neural_network.py
+++ neural_network.py
@@ -8,10 +8,10 @@ class NeuralNetwork:
         self.neurons = {
             0: [
                 Neuron(
-                    w=np.random.rand(neurons_per_layer[0]),
-                    b=np.random.rand(1)
+                    w=np.eye(neurons_per_layer[0])[i],
+                    b=np.zeros(1)
                 )
-            for _ in range(neurons_per_layer[0])],
+            for i in range(neurons_per_layer[0])],
             **{
                 layer: [
                     Neuron(
@@ -21,27 +21,20 @@ class NeuralNetwork:
                 for _ in range(neurons_per_layer[layer])]
             for layer in range(1, layers)},
         }
-        self.all_w_list, self.all_b_list = {layer: None for layer in range(layers)}, {layer: None for layer in range(layers)}
+        self.all_w_list, self.all_b_list = {layer: None for layer in range(1, layers)}, {layer: None for layer in range(1, layers)}
         self.all_a_list, self.all_h_list = {layer: None for layer in range(layers)}, {layer: None for layer in range(layers)}
-        self.all_delta_a_list, self.all_delta_h_list = {layer: None for layer in range(layers)}, {layer: None for layer in range(layers)}
-        self.all_delta_w_list, self.all_delta_b_list = {layer: None for layer in range(layers)}, {layer: None for layer in range(layers)}
-        self.yhat = None
         
     def feedforward(self, x):
         x = np.array(x).reshape(-1,)
 
         # feedforward for the first / input layer
-        a_layerwise, h_layerwise, w_layerwise, b_layerwise = [], [], [], []
+        a_layerwise, h_layerwise = [], []
         for neuron in self.neurons[0]:
             neuron.feedforward(x)
             a_layerwise.append(neuron.a)
             h_layerwise.append(neuron.h)
-            w_layerwise.append(neuron.w)
-            b_layerwise.append(neuron.b)
         self.all_a_list[0] = np.array(a_layerwise)
         self.all_h_list[0] = np.array(h_layerwise)
-        self.all_w_list[0] = np.array(w_layerwise)
-        self.all_b_list[0] = np.array(b_layerwise)
 
         # feedforward for the rest of the layers (hidden and output)
         for layer in range(1, self.layers):
@@ -59,65 +52,76 @@ class NeuralNetwork:
         
         h_final = self.all_h_list[self.layers-1]
         h_final_exp_sum = np.sum(np.exp(h_final))
-        self.y_hat = np.exp(h_final) / h_final_exp_sum
 
-        return self.y_hat
+        return np.exp(h_final) / h_final_exp_sum
     
 
     def cross_entropy_loss(self, class_label):
         return -np.log(self.y_hat[class_label])
     
-    # gradient wrt to h
-    def gradient_wrt_h(self, w_next, gradient_a_next):
-        return np.dot(w_next.T, gradient_a_next)
 
-    # gradient wrt to a
-    def gradient_wrt_a(self, gradient_h, a):
-        def sigmoid_prime(a):
-            return a * (1 - a)
-        
-        return gradient_h * sigmoid_prime(a)
-
-    # gradient wrt to weights
-    def gradient_wrt_w(self, gradient_a, h):
-        return np.outer(gradient_a, h.T)
-
-    # gradient wrt to bias
-    def gradient_wrt_b(self, gradient_a):
-        return gradient_a
-    
-
-    # gradient wrt to outer layer
-    def gradient_wrt_output(self, class_label):
-        a_delta = self.y_hat.copy()
-        a_delta[class_label] -= 1
-        self.all_delta_a_list[self.layers-1] = a_delta
-        
-        b_delta = self.gradient_wrt_b(a_delta)
-        self.all_delta_b_list[self.layers-1] = b_delta
-
-        w_delta = self.gradient_wrt_w(a_delta, self.all_h_list[self.layers-1])   
-        self.all_delta_w_list[self.layers-1] = w_delta
-
-    # gradient wrt to hidden layers and input layer
-    def gradient_wrt_hidden(self):
-        for layer in range(self.layers-2, 0, -1):
-            h_delta = self.gradient_wrt_h(self.all_w_list[layer+1], self.all_delta_a_list[layer+1])
-            self.all_delta_h_list[layer] = h_delta
-
-            a_delta = self.gradient_wrt_a(h_delta, self.all_a_list[layer])
-            self.all_delta_a_list[layer] = a_delta
-
-            b_delta = self.gradient_wrt_b(a_delta)
-            self.all_delta_b_list[layer] = b_delta
-
-            w_delta = self.gradient_wrt_w(a_delta, self.all_h_list[layer-1])
-            self.all_delta_w_list[layer] = w_delta
-    
     # backpropagation
-    def backprop(self, class_label):
-        self.gradient_wrt_output(class_label)
-        self.gradient_wrt_hidden()
+    def backprop(self, all_w_list, all_b_list, y_hat, class_label):
+        all_delta_a_list, all_delta_h_list = {layer: None for layer in range(1, self.layers)}, {layer: None for layer in range(1, self.layers)}
+        all_delta_w_list, all_delta_b_list = {layer: None for layer in range(1, self.layers)}, {layer: None for layer in range(1, self.layers)}
+        
+
+        # gradient wrt to h
+        def gradient_wrt_h(w_next, gradient_a_next):
+            return np.dot(w_next.T, gradient_a_next)
+
+        # gradient wrt to a
+        def gradient_wrt_a(gradient_h, a):
+            return gradient_h * a * (1 - a)
+
+        # gradient wrt to weights
+        def gradient_wrt_w(gradient_a, h):
+            return np.outer(gradient_a, h.T)
+
+        # gradient wrt to bias
+        def gradient_wrt_b(gradient_a):
+            return gradient_a
+        
+
+        # gradient wrt to outer layer
+        def gradient_wrt_output(self, y_hat, class_label):
+            a_delta = y_hat.copy()
+            a_delta[class_label] -= 1
+            all_delta_a_list[self.layers-1] = a_delta
+            
+            b_delta = gradient_wrt_b(a_delta)
+            all_delta_b_list[self.layers-1] = b_delta
+
+            w_delta = gradient_wrt_w(a_delta, self.all_h_list[self.layers-2])   
+            all_delta_w_list[self.layers-1] = w_delta
+
+        # gradient wrt to hidden layers
+        def gradient_wrt_hidden(self):
+            for layer in range(self.layers-2, 0, -1):
+                h_delta = gradient_wrt_h(all_w_list[layer+1], all_delta_a_list[layer+1])
+                all_delta_h_list[layer] = h_delta
+
+                a_delta = gradient_wrt_a(h_delta, self.all_a_list[layer])
+                all_delta_a_list[layer] = a_delta
+
+                b_delta = gradient_wrt_b(a_delta)
+                all_delta_b_list[layer] = b_delta
+
+                w_delta = gradient_wrt_w(a_delta, self.all_h_list[layer-1])
+                all_delta_w_list[layer] = w_delta
+
+
+        gradient_wrt_output(self, y_hat, class_label)
+        gradient_wrt_hidden(self)
+        return all_delta_w_list, all_delta_b_list
+    
+    # after gradient calculation, update weights
+    def update_weights(self, delta_w, delta_b):
+        for layer in range(1, self.layers):
+            d_w, d_b = delta_w[layer], delta_b[layer]
+            for i in range(len(self.neurons[layer])):
+                self.neurons[layer][i].w -= d_w[i]
+                self.neurons[layer][i].b -= d_b[i]
 
     def __repr__(self):
         return f'''NeuralNetwork(Layers = {self.layers}, Neurons per layer = {self.neurons_per_layer})'''
